3fb4d975a0da36f58ba13e8f475f9ae57b9c2870
Divya Sasidharan <hello@shortdiv.com>
2019-06-10 18:01:09 -0400

    checking proper cookie is parsed and sent

--- functions/redirect/redirect.js
+++ functions/redirect/redirect.js
@@ -10,14 +10,25 @@ exports.handler = function(event, context, callback) {
   const cookieHeader = headers.cookie || "";
   const cookies = cookie.parse(cookieHeader);
 
+  // for some reason nf_jwt is duplicated, one with null another with val, this removes the dup //
+  const headerCookies = headers.cookie.split(";");
+  let headerz = "";
+  for (var i = 0; i < headerCookies.length; i++) {
+    if (headerCookies[i].split("=")[1] !== "null") {
+      headerz += headerCookies[i] + ";";
+    }
+  }
+
+  const parsecookie = cookie.parse(headerz);
+
   callback(null, {
-    statusCode: 302,
+    statusCode: 200,
     headers: {
       Location: `${redirectBaseUrl}/.netlify/functions/read-cookie?cookie=${
-        cookies.nf_jwt
+        parsecookie.nf_jwt
       }`,
       "Cache-Control": "no-cache"
     },
-    body: JSON.stringify({ cookie: cookies.nf_jwt })
+    body: JSON.stringify({ cookie: cookies, parsed: parsecookie.nf_jwt })
   });
 };
