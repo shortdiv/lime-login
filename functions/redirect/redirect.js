7d1ce5d2c4afccaa4de97984b5d4c1acbe70c77c
Divya Sasidharan <hello@shortdiv.com>
2019-06-10 16:26:10 -0400

    check cookie

--- functions/redirect/redirect.js
+++ functions/redirect/redirect.js
@@ -1,22 +1,19 @@
-const parseURL = require("url-parse");
+// const parseURL = require("url-parse");
 const cookie = require("cookie");
 
 exports.handler = function(event, context, callback) {
-  const params = event.queryStringParameters;
-  const urlData = parseURL(params.site);
-  const redirectBaseUrl = urlData.origin;
+  // const params = event.queryStringParameters;
+  // const urlData = parseURL(params.site);
+  // const redirectBaseUrl = urlData.origin;
 
   const { headers } = event;
   const cookieHeader = headers.cookie || "";
   const cookies = cookie.parse(cookieHeader);
 
   callback(null, {
-    statusCode: 302,
+    statusCode: 200,
     headers: {
-      "Set-Cookie": cookies.nf_jwt,
-      Location: `${redirectBaseUrl}/.netlify/functions/read-cookie?cookie=${
-        cookies.nf_jwt
-      }`,
+      "Content-Type": "text/html",
       "Cache-Control": "no-cache"
     },
     body: JSON.stringify({ cookie: cookies.nf_jwt })
